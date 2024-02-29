e1f73f0294d3f2e8b771e014cc47ef2b100d850a
Ercüment Cetin <ercu2810@gmail.com>
2024-02-29 17:00:04 +0100

    change keymap and add funny plugins

--- lua/plugins/funny.lua
+++ lua/plugins/funny.lua
@@ -1,13 +1,20 @@
 return {
-    {
-        "eandrju/cellular-automaton.nvim",
+	{
+		"eandrju/cellular-automaton.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>cmr", "<cmd>CellularAutomaton make_it_rain<CR>")
+			vim.keymap.set("n", "<leader>cgl", "<cmd>CellularAutomaton game_of_life<CR>")
+			vim.keymap.set("n", "<leader>csm", "<cmd>CellularAutomaton scramble<CR>")
+		end,
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+	},
+	{
+		"jim-fx/sudoku.nvim",
+        cmd = "Sudoku",
         config = function()
-            vim.keymap.set("n", "<leader>cmr", "<cmd>CellularAutomaton make_it_rain<CR>")
-            vim.keymap.set("n", "<leader>cgl", "<cmd>CellularAutomaton game_of_life<CR>")
-            vim.keymap.set("n", "<leader>csm", "<cmd>CellularAutomaton scramble<CR>")
+            require("sudoku").setup({})
         end
-    },
-    {
-        "ThePrimeagen/vim-be-good"
-    }
+	},
 }
--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -10,7 +10,7 @@ return {
 				theme_conf = { border = true },
 				previewer = false,
 			},
-			vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+			vim.keymap.set("n", "<leader>sl", require("auto-session.session-lens").search_session, {
 				noremap = true,
 			}),
 		})
