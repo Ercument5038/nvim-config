153213d41a0ce3b41d4568a80e008f24bcc15539
Ercument5038 <ercu2810@gmail.com>
2024-02-29 15:03:47 +0100

    add funny plugins

--- lua/plugins/funny.lua
+++ lua/plugins/funny.lua
@@ -1,13 +1,19 @@
 return {
     {
+        "efueyo/td.nvim",
+    },
+    {
+        "alec-gibson/nvim-tetris",
+    },
+    {
         "eandrju/cellular-automaton.nvim",
         config = function()
             vim.keymap.set("n", "<leader>cmr", "<cmd>CellularAutomaton make_it_rain<CR>")
             vim.keymap.set("n", "<leader>cgl", "<cmd>CellularAutomaton game_of_life<CR>")
             vim.keymap.set("n", "<leader>csm", "<cmd>CellularAutomaton scramble<CR>")
-        end
+        end,
     },
     {
-        "ThePrimeagen/vim-be-good"
-    }
+        "ThePrimeagen/vim-be-good",
+    },
 }
