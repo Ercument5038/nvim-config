3887a8e728c8a6b568129c1db367f7aecb19e6cb
Ercüment Cetin <ercu2810@gmail.com>
2024-03-23 16:17:54 +0100

    change color on highlight on yank

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,7 +1,11 @@
+-- Define a custom highlight group for yank feedback
+vim.api.nvim_command("hi YankHighlight guifg=#000000 guibg=#ff9999")
+
+-- Create an autocmd for TextYankPost event to apply the highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.highlight.on_yank({ higroup = "YankHighlight" })
 	end,
 })
