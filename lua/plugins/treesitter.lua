c680c26919f6475d767f25d548858b4405893bc4
Ercument5038 <ercu2810@gmail.com>
2025-12-18 17:22:45 +0100

    switch to main branch of nvim-treesitter and keep backup of master config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,16 @@
+---@module "lazy"
+---@type LazySpec
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		opts = {
-			ensure_installed = {
+		config = function()
+			local ts = require("nvim-treesitter")
+
+			-- Install core parsers at startup
+			ts.install({
 				"css",
 				"html",
 				"javascript",
@@ -15,31 +21,50 @@ return {
 				"lua",
 				"vim",
 				"vimdoc",
-				"c_sharp",
 				"bash",
-			},
-			snyc_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-		},
+				"lua",
+				"luadoc",
+				"typst",
+			})
+
+			local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+
+			local ignore_filetypes = {
+				"checkhealth",
+				"mason",
+				"lazy",
+			}
+
+			-- Auto-install parsers and enable highlighting on FileType
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				desc = "Enable treesitter highlighting and indentation",
+				callback = function(event)
+					if vim.tbl_contains(ignore_filetypes, event.match) then
+						return
+					end
+
+					local lang = vim.treesitter.language.get_lang(event.match) or event.match
+					local buf = event.buf
+
+					-- Start highlighting immediatley (works if parser exists)
+					pcall(vim.treesitter.start, buf, lang)
+
+					-- Enable treesitter indentation
+					vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+					-- Install missing parsers (async, no-op if already installed)
+					ts.install({ lang })
+				end,
+			})
+		end,
 	},
 	{
 		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-ts-autotag").setup({
-				opts = {
-					enable_close = true, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
-					enable_close_on_slash = false, -- Auto close on trailing </
-				},
-			})
-		end,
+		opts = {
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
+		},
 	},
 }
