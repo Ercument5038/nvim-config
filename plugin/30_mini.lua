fe61e0ab8b45ef7c01816ce00a69996fef370e77
Ercument5038 <ercu2810@gmail.com>
2026-03-18 05:48:12 +0300

    switch to mini.completion from blink.cmp

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -16,6 +16,7 @@ now(function()
 	local ext3_blocklist = { scm = true, txt = true, yml = true }
 	local ext4_blocklist = { json = true, yaml = true }
 	require("mini.icons").setup({
+
 		use_file_extension = function(ext, _)
 			return not (ext3_blocklist[ext:sub(-3)] or ext4_blocklist[ext:sub(-4)])
 		end,
@@ -149,34 +150,35 @@ later(function()
 	vim.api.nvim_set_hl(0, "MiniCursorword", { link = "LspReferenceText" })
 end)
 
--- now_if_args(function()
---  -- Customize post-processing of LSP responses for a better user experience.
---  -- Don't show 'Text' suggestions (usually noisy) and show snippets last.
---  local process_items_opts = { kind_priority = { Text = -1, Snippet = 99 } }
---  local process_items = function(items, base)
---    return MiniCompletion.default_process_items(items, base, process_items_opts)
---  end
---  require("mini.completion").setup({
---    lsp_completion = {
---      -- Without this config autocompletion is set up through `:h 'completefunc'`.
---      -- Although not needed, setting up through `:h 'omnifunc'` is cleaner
---      -- (sets up only when needed) and makes it possible to use `<C-u>`.
---      source_func = "omnifunc",
---      auto_setup = false,
---      process_items = process_items,
---    },
---  })
---
---  -- Set 'omnifunc' for LSP completion only when needed.
---  local on_attach = function(ev)
---    vim.bo[ev.buf].omnifunc = "v:lua.MiniCompletion.completefunc_lsp"
---  end
---  Config.new_autocmd("LspAttach", nil, on_attach, "Set 'omnifunc'")
---
---  -- Advertise to servers that Neovim now supports certain set of completion and
---  -- signature features through 'mini.completion'.
---  vim.lsp.config("*", { capabilities = MiniCompletion.get_lsp_capabilities() })
--- end)
+now_if_args(function()
+	-- Customize post-processing of LSP responses for a better user experience.
+	-- Don't show 'Text' suggestions (usually noisy) and show snippets last.
+	local process_items_opts = { kind_priority = { Text = -1, Snippet = 99 } }
+	local process_items = function(items, base)
+		return MiniCompletion.default_process_items(items, base, process_items_opts)
+	end
+	require("mini.completion").setup({
+
+		lsp_completion = {
+			-- Without this config autocompletion is set up through `:h 'completefunc'`.
+			-- Although not needed, setting up through `:h 'omnifunc'` is cleaner
+			-- (sets up only when needed) and makes it possible to use `<C-u>`.
+			source_func = "omnifunc",
+			auto_setup = false,
+			process_items = process_items,
+		},
+	})
+
+	-- Set 'omnifunc' for LSP completion only when needed.
+	local on_attach = function(ev)
+		vim.bo[ev.buf].omnifunc = "v:lua.MiniCompletion.completefunc_lsp"
+	end
+	Config.new_autocmd("LspAttach", nil, on_attach, "Set 'omnifunc'")
+
+	-- Advertise to servers that Neovim now supports certain set of completion and
+	-- signature features through 'mini.completion'.
+	vim.lsp.config("*", { capabilities = MiniCompletion.get_lsp_capabilities() })
+end)
 
 later(function()
 	require("mini.diff").setup()
