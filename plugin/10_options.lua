9adfdfe0e81ca001a8061ac970d916eb62ff361f
Ercument5038 <ercu2810@gmail.com>
2026-03-18 05:47:24 +0300

    new 0.12 options

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -26,11 +26,14 @@ end
 
 -- UI =========================================================================
 vim.o.breakindent = true -- Indent wrapped lines to match line start
+vim.o.breakindentopt = "list:-1" -- Add padding for lists (if 'wrap' is set)
 vim.o.colorcolumn = "80" -- Draw column at 80
 vim.o.cursorline = true -- Enable current line highlighting
 vim.o.linebreak = true -- Wrap lines at 'breakat' (if 'wrap' is set)
 vim.o.number = true -- Show line numbers
+vim.o.pumborder = "single"
 vim.o.pumheight = 10 -- Make popup menu smaller
+vim.o.pummaxwidth = 100 -- Make popup menu not too wide
 vim.o.relativenumber = false -- Dont Show relative numbers
 vim.o.scrolloff = 10 -- Set scrolloff to 10 lines
 vim.o.shortmess = "CFOSWacosI" -- Disable some built-in completion messages
@@ -74,6 +77,11 @@ vim.o.virtualedit = "block" -- Allow going past end of line in blockwise mode
 vim.o.iskeyword = "@,48-57,_,192-255,-" -- Treat dash as `word` textobject part
 
 vim.o.inccommand = "split" -- Show preview when substituting
+
+-- Built-in completion
+vim.o.complete = ".,w,b,kspell" -- Use less sources
+vim.o.completeopt = "menuone,noselect,fuzzy,nosort" -- Use custom behavior
+vim.o.completetimeout = 100 -- Limit sources delay
 
 -- Autocommands ===============================================================
 
